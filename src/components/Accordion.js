7ff1b94a702644ba3432cf37607c406088fc348d
Chance Strickland <hi@chancedigital.io>
2020-03-19 11:28:59 -0700

    cool

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,164 +1,97 @@
-import React, { createContext, useContext, useRef } from "react";
+import React from "react";
 import { useId } from "@reach/auto-id";
-import {
-  useDescendant,
-  useDescendants,
-  createDescendantContext
-} from "../utils/descendants";
 import "./Accordion.css";
-
-const DescendantContext = createDescendantContext();
-const AccordionContext = createContext();
-const AccordionItemContext = createContext();
-const useDescendantContext = () => useContext(DescendantContext);
-const useAccordionContext = () => useContext(AccordionContext);
-const useAccordionItemContext = () => useContext(AccordionItemContext);
 
 ////////////////////////////////////////////////////////////////////////////////
 
-export function Accordion({ children, index, onChange, ...props }) {
+export function Accordion({ children, index, onChange, data, ...props }) {
   let id = useId(props.id);
-  let [items, setItems] = useDescendants();
-
-  let context = {
-    accordionId: id,
-    accordionIndex: index,
-    onChange,
-    onSelectPanel(newIndex, triggerNode) {
-      triggerNode && triggerNode.focus();
-      onChange(newIndex);
-    }
-  };
 
   return (
-    <DescendantContext.DescendantProvider items={items} set={setItems}>
-      <AccordionContext.Provider value={context}>
-        <div {...props} data-accordion="">
-          {children}
-        </div>
-      </AccordionContext.Provider>
-    </DescendantContext.DescendantProvider>
+    <div {...props} data-accordion="">
+      {data.map((accordionItem, itemIndex) => {
+        let state = index === itemIndex ? "open" : "closed";
+        return (
+          <AccordionItem
+            accordionId={id}
+            accordionData={data}
+            itemData={accordionItem}
+            index={itemIndex}
+            state={state}
+            onChange={onChange}
+            onSelectPanel={(newIndex, triggerNode) => {
+              triggerNode && triggerNode.focus();
+              onChange(newIndex);
+            }}
+          />
+        );
+      })}
+    </div>
   );
 }
 
-export function AccordionItem({ children, ...props }) {
-  let { accordionId, accordionIndex } = useAccordionContext();
-  let buttonRef = useRef(null);
-
-  let index = useDescendant({
-    context: DescendantContext,
-    element: buttonRef.current
-  });
-
+export function AccordionItem({
+  accordionId,
+  index,
+  state,
+  accordionData,
+  itemData,
+  onChange,
+  onSelectPanel,
+  ...props
+}) {
   // We need unique IDs for the panel and button to point to one another
   let itemId = `${accordionId}-${index}`;
   let panelId = `panel-${itemId}`;
   let buttonId = `button-${itemId}`;
 
-  let state = accordionIndex === index ? "open" : "closed";
-
-  let context = {
-    buttonId,
-    index,
-    itemId,
-    buttonRef,
-    panelId,
-    state
-  };
-
-  return (
-    <AccordionItemContext.Provider value={context}>
-      <div {...props} data-accordion-item="" data-state={state}>
-        {children}
-      </div>
-    </AccordionItemContext.Provider>
-  );
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-export function AccordionButton({
-  children,
-  onClick,
-  onKeyDown,
-  tabIndex,
-  ...props
-}) {
-  let { descendants: accordionItems } = useDescendantContext();
-  let { onSelectPanel } = useAccordionContext();
-
-  let {
-    buttonId,
-    buttonRef: ref,
-    index,
-    panelId,
-    state
-  } = useAccordionItemContext();
-
-  function handleClick(event) {
-    event.preventDefault();
-    onSelectPanel(index, ref.current);
-  }
-
   function handleKeyDown(event) {
     let navIndex = -1;
 
-    function getNextOption() {
-      let atBottom = index === accordionItems.length - 1;
-      return atBottom
-        ? getFirstOption()
-        : accordionItems[(index + 1) % accordionItems.length];
+    function getNextIndex() {
+      let atBottom = index === accordionData.length - 1;
+      return atBottom ? getFirstIndex() : (index + 1) % accordionData.length;
     }
 
-    function getPreviousOption() {
+    function getPreviousIndex() {
       let atTop = index === 0;
       return atTop
-        ? getLastOption()
-        : accordionItems[
-            (index - 1 + accordionItems.length) % accordionItems.length
-          ];
+        ? getLastIndex()
+        : (index - 1 + accordionData.length) % accordionData.length;
     }
 
-    function getFirstOption() {
-      return accordionItems[0];
+    function getFirstIndex() {
+      return 0;
     }
 
-    function getLastOption() {
-      return accordionItems[accordionItems.length - 1];
+    function getLastIndex() {
+      return accordionData.length - 1;
     }
 
     switch (event.key) {
       case "ArrowUp":
         event.preventDefault();
-        let prev = getPreviousOption();
-        navIndex = prev ? prev.index : -1;
+        navIndex = getPreviousIndex();
         break;
       case "ArrowDown":
         event.preventDefault();
-        let next = getNextOption();
-        navIndex = next ? next.index : -1;
+        navIndex = getNextIndex();
         break;
       case "PageUp":
         event.preventDefault();
-        let prevOrFirst = (event.ctrlKey
-          ? getPreviousOption
-          : getFirstOption)();
-        navIndex = prevOrFirst ? prevOrFirst.index : -1;
+        navIndex = (event.ctrlKey ? getPreviousIndex : getFirstIndex)();
         break;
       case "PageDown":
         event.preventDefault();
-        let nextOrLast = (event.ctrlKey ? getNextOption : getLastOption)();
-        navIndex = nextOrLast ? nextOrLast.index : -1;
+        navIndex = (event.ctrlKey ? getNextIndex : getLastIndex)();
         break;
       case "Home":
         event.preventDefault();
-        let first = getFirstOption();
-        navIndex = first ? first.index : -1;
+        navIndex = getFirstIndex();
         break;
       case "End":
         event.preventDefault();
-        let last = getLastOption();
-        navIndex = last ? last.index : -1;
+        navIndex = getLastIndex();
         break;
       default:
         break;
@@ -166,62 +99,39 @@ export function AccordionButton({
 
     if (navIndex > -1) {
       // eslint-disable-next-line no-unused-expressions
-      accordionItems[navIndex].element?.focus();
+      accordionData[navIndex]?.ref?.current?.focus();
     }
   }
 
-  return (
-    <button
-      aria-controls={panelId}
-      aria-expanded={state === "open"}
-      {...props}
-      ref={ref}
-      data-accordion-button=""
-      id={buttonId}
-      onClick={wrapEvent(onClick, handleClick)}
-      onKeyDown={wrapEvent(onKeyDown, handleKeyDown)}
-    >
-      {children}
-    </button>
-  );
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-export function AccordionPanel({ as: Comp = "div", children, ...props }) {
-  const { panelId, buttonId, state } = useAccordionItemContext();
+  function handleClick(event) {
+    event.preventDefault();
+    onSelectPanel(index, itemData.ref.current);
+  }
 
   return (
-    <Comp
-      hidden={state !== "open"}
-      role="region"
-      aria-labelledby={buttonId}
-      {...props}
-      data-accordion-panel=""
-      data-state={state}
-      id={panelId}
-      tabIndex={-1}
-    >
-      {children}
-    </Comp>
+    <div {...props} data-accordion-item="" data-state={state}>
+      <button
+        aria-controls={panelId}
+        aria-expanded={state === "open"}
+        ref={itemData.ref}
+        data-accordion-button=""
+        id={buttonId}
+        onClick={handleClick}
+        onKeyDown={handleKeyDown}
+      >
+        {itemData.buttonText}
+      </button>
+      <div
+        hidden={state !== "open"}
+        role="region"
+        aria-labelledby={buttonId}
+        data-accordion-panel=""
+        data-state={state}
+        id={panelId}
+        tabIndex={-1}
+      >
+        {itemData.panelText}
+      </div>
+    </div>
   );
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * Wraps a lib-defined event handler and a user-defined event handler, returning
- * a single handler that allows a user to prevent lib-defined handlers from
- * firing.
- *
- * @param {Function} theirHandler User-supplied event handler
- * @param {Function} ourHandler Library-supplied event handler
- */
-function wrapEvent(theirHandler, ourHandler) {
-  return event => {
-    theirHandler && theirHandler(event);
-    if (!event.defaultPrevented) {
-      return ourHandler(event);
-    }
-  };
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,48 @@
-import React, { useState } from "react";
-import {
-  Accordion,
-  AccordionButton,
-  AccordionItem,
-  AccordionPanel
-} from "./Accordion";
+import React, { useRef, useState } from "react";
+import { Accordion } from "./Accordion";
 import "./App.css";
 
 function App() {
   let [index, setIndex] = useState(0);
+  let data = [
+    {
+      buttonText: `Hello darkness, my old friend`,
+      panelText: `Fringilla porta porttitor tristique praesent habitant, habitasse
+      ut faucibus congue arcu nullam, metus montes duis fermentum. Dui
+      cursus proin dignissim suspendisse lobortis faucibus
+      pellentesque, curae integer facilisi lorem ad class condimentum
+      maecenas, phasellus mus sagittis non diam taciti.`,
+      ref: useRef(),
+    },
+    {
+      buttonText: `I've come to talk with you again`,
+      panelText: `Fringilla porta porttitor tristique praesent habitant, habitasse
+      ut faucibus congue arcu nullam, metus montes duis fermentum. Cum
+      faucibus turpis risus hendrerit mi volutpat laoreet, netus justo
+      consectetur vivamus nunc per, dui at leo nascetur sociis taciti.`,
+      ref: useRef(),
+    },
+    {
+      buttonText: `Because a vision softly creeping`,
+      panelText: `Integer dui nostra vehicula sit augue metus faucibus aliquam
+      vulputate, curae per himenaeos erat facilisi lorem consectetur
+      penatibus. Penatibus vel mattis potenti orci libero augue aenean
+      posuere, adipiscing dapibus eros laoreet dui sagittis
+      condimentum, ut pellentesque curae natoque aliquet velit
+      integer.`,
+      ref: useRef(),
+    },
+    {
+      buttonText: `Left its seeds while I was sleeping`,
+      panelText: `Netus nulla iaculis dolor sociosqu commodo mollis congue sociis et fames, est adipiscing venenatis dictum donec himenaeos porttitor molestie nam, in fusce consectetur luctus tincidunt elit sed feugiat conubia. Facilisis euismod vehicula phasellus fames lorem montes proin egestas, accumsan molestie potenti sociis erat nam sit orci fringilla, turpis metus volutpat vulputate a varius hendrerit.`,
+      ref: useRef(),
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <Accordion index={index} onChange={setIndex}>
-            <AccordionItem>
-              <AccordionButton>Hello darkness, my old friend</AccordionButton>
-              <AccordionPanel>
-                Fringilla porta porttitor tristique praesent habitant, habitasse
-                ut faucibus congue arcu nullam, metus montes duis fermentum. Dui
-                cursus proin dignissim suspendisse lobortis faucibus
-                pellentesque, curae integer facilisi lorem ad class condimentum
-                maecenas, phasellus mus sagittis non diam taciti.
-              </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem disabled>
-              <AccordionButton>
-                I've come to talk with you again
-              </AccordionButton>
-              <AccordionPanel>
-                Fringilla porta porttitor tristique praesent habitant, habitasse
-                ut faucibus congue arcu nullam, metus montes duis fermentum. Cum
-                faucibus turpis risus hendrerit mi volutpat laoreet, netus justo
-                consectetur vivamus nunc per, dui at leo nascetur sociis taciti.
-              </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem>
-              <AccordionButton>
-                Because a vision softly creeping
-              </AccordionButton>
-              <AccordionPanel>
-                Integer dui nostra vehicula sit augue metus faucibus aliquam
-                vulputate, curae per himenaeos erat facilisi lorem consectetur
-                penatibus. Penatibus vel mattis potenti orci libero augue aenean
-                posuere, adipiscing dapibus eros laoreet dui sagittis
-                condimentum, ut pellentesque curae natoque aliquet velit
-                integer.
-              </AccordionPanel>
-            </AccordionItem>
-          </Accordion>
+          <Accordion index={index} onChange={setIndex} data={data} />
         </div>
       </header>
     </div>
